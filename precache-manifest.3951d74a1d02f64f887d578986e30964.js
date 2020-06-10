5f8ab87cd88124ef4f163f7ebf0e0d78bb6dce14
avdeevaelena5@gmail.com <avdeevaelena5@gmail.com>
2020-06-10 14:23:10 +0300

    Updates

---
+++ precache-manifest.3951d74a1d02f64f887d578986e30964.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f363ed2c58f34d125b8c051ed52984",
+    "url": "/testGHP/index.html"
+  },
+  {
+    "revision": "76e5178ab49237ff170f",
+    "url": "/testGHP/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56a24f0fdc3dc46ba353",
+    "url": "/testGHP/static/js/2.fb271edb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testGHP/static/js/2.fb271edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e5178ab49237ff170f",
+    "url": "/testGHP/static/js/main.e457c0db.chunk.js"
+  },
+  {
+    "revision": "c1b8ca3f12406e6f4f03",
+    "url": "/testGHP/static/js/runtime-main.b0c818b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testGHP/static/media/logo.5d5d9eef.svg"
+  }
+]);
